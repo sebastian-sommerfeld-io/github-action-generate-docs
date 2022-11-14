e45a5c36bf0e32b8c1455c5243724dafc007d66d
sebastian <sebastian@sommerfeld.io>
2022-11-14 23:08:47 +0100

    fix: hadolint violation

--- build.sh
+++ build.sh
@@ -6,8 +6,3 @@
   docker pull sommerfeldio/git:ci-build
   docker build -t local/shdoc:dev .
 )
-
-docker run --rm \
-  --volume "$(pwd):$(pwd)" \
-  --workdir "$(pwd)" \
-  local/shdoc:dev shdoc < entrypoint.sh > entrypoint.md
